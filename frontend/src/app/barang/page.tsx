7a347ec3a0c7eb031b2def1bc98b23e2a3b85850
Edbertt <edbert1xie@gmail.com>
2025-04-15 10:17:35 +0700

    Menambah Header dan page Barang

---
+++ frontend/src/app/barang/page.tsx
@@ -0,0 +1,11 @@
+import { InvoiceTable } from "@/components/Tables/invoice-table";
+
+export default function Barang() {
+    return(
+        <>
+            <div className="p-8">
+                <InvoiceTable />
+            </div>
+        </>
+    )
+}
